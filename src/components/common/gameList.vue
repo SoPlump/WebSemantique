73bb5a9437ff2464c1ca0a8af5b1cf83f8ad305a
bvachez <brice.vachez@insa-lyon.fr>
2019-12-13 08:18:29 +0100

    Components for serie

---
+++ src/components/common/gameList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script src='https://code.jquery.com/jquery-3.4.1.min.js'>
+    export default {
+        name: "gameList"
+    }
+</script>
+
+<style scoped>
+
+</style>
