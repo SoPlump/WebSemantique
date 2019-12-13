5ba9fac9273f22101597a9a9c259048fe940ee17
Pacôme <pacome.lehoux@laposte.net>
2019-12-13 11:20:24 +0100

    added sparql queries for studios (developers and publishers)

--- src/dbpedia-query.js
+++ src/dbpedia-query.js
@@ -85,3 +85,118 @@ export const getAllGenresByName = name => {
             console.error(error);
         });
 };
+
+export const getStudioByName = name => {
+    return sparql
+        .query(`
+            SELECT ?res ?name ?abstract WHERE {
+                ?res a ?type;
+                dbo:abstract ?abstract;
+                foaf:name ?name.
+                FILTER (?res = <http://dbpedia.org/resource/${name}>)
+                FILTER(LangMatches(lang(?abstract), "en"))
+                FILTER(LangMatches(lang(?name), "en"))
+                FILTER(?type = yago:WikicatVideoGamePublishers || ?type = yago:WikicatVideoGameDevelopmentCompanies)
+            }
+        `)
+        .then(res => {
+            const studio = res.results.bindings[0];
+            if(studio) {
+                return new Promise(resolve => resolve({
+                    name: studio.name.value,
+                    abstract: studio.abstract.value,
+                    uri: studio.res.value
+                }));
+            }
+            return null;
+        })
+        .then(studio => {
+            if(studio) {
+                return sparql
+                    .query(`
+                    SELECT ?res ?name WHERE {
+                        ?res a dbo:VideoGame;
+                        dbo:genre ?genre;
+                        rdfs:label ?name;
+                        dbo:developer ?developer.
+                        FILTER(?developer = <${studio.uri}>)
+                        FILTER(LangMatches(lang(?name), "en"))
+                    } LIMIT 10
+                    `)
+                    .then(res => {
+                        const games = res.results.bindings;
+                        return new Promise(resolve => resolve({
+                            ...studio,
+                            developedGames: games.map(game => {
+                                return {
+                                    name: game.name.value,
+                                    uri: game.res.value
+                                }
+                            })
+                        }));
+                    });
+            }
+            return null;
+        })
+        .then(studio => {
+            if(studio) {
+                return sparql
+                    .query(`
+                    SELECT ?res ?name WHERE {
+                        ?res a dbo:VideoGame;
+                        dbo:genre ?genre;
+                        rdfs:label ?name;
+                        dbo:publisher ?publisher.
+                        FILTER(?publisher = <${studio.uri}>)
+                        FILTER(LangMatches(lang(?name), "en"))
+                    } LIMIT 10
+                    `)
+                    .then(res => {
+                        const games = res.results.bindings;
+                        return new Promise(resolve => resolve({
+                            ...studio,
+                            publishedGames: games.map(game => {
+                                return {
+                                    name: game.name.value,
+                                    uri: game.res.value
+                                }
+                            })
+                        }));
+                    });
+            }
+            return null;
+        })
+        .catch(error => {
+            /*eslint-disable no-console*/
+            console.error(error);
+        });
+};
+
+export const getAllStudiosByName = name => {
+    return sparql
+        .query(`
+            SELECT ?res ?name WHERE {
+                ?res a ?type;
+                foaf:name ?name.
+                FILTER contains(lcase(?name), lcase("${name}"))
+                FILTER(LangMatches(lang(?name), "en"))
+                FILTER(?type = yago:WikicatVideoGamePublishers || ?type = yago:WikicatVideoGameDevelopmentCompanies)
+            }
+        `)
+        .then(res => {
+            let studios = res.results.bindings;
+            return new Promise(resolve => {
+                studios = studios.map(studio => {
+                    return {
+                        name: studio.name.value,
+                        res: studio.res.value
+                    };
+                });
+                resolve(studios);
+            });
+        })
+        .catch(error => {
+            /*eslint-disable no-console*/
+            console.error(error);
+        });
+};
