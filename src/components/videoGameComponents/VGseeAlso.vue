f031334fcc273d57130c4a7e375c21fa57e1270a
Guilhem Cerba <guilhem.cerba@insa-lyon.fr>
2019-12-13 09:34:57 +0100

    commentaires pour le css

--- src/components/videoGameComponents/VGseeAlso.vue
+++ src/components/videoGameComponents/VGseeAlso.vue
@@ -1,5 +1,7 @@
 <template>
+    <!-- Pour le CSS voir plus tard -->
     <div>
+        <!-- Modifier les div suivantes en gameList une fois à jour avec master -->
         <div id = "sameGenre">Games of the same genre : </div>
         <div id = "sameSerie">Games of the same serie : </div>
     </div>
