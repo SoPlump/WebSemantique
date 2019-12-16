0a62f3a3b976a1c9105e233cd8a8afa0b9cf7291
mathieu <matmont98@gmail.com>
2019-12-16 15:57:32 +0100

    Style for serie

--- src/components/GenreView.vue
+++ src/components/GenreView.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-        <div>
-            Genre : {{genreName}}
-        </div>
-        <div>
-            Abstract : {{abstract}}
+    <div class="flex flex-col">
+        <div class="mb-10">
+            <h1 class="text-4xl p-4 rounded-lg border-black border mb-3">{{name}}</h1>
         </div>
 
-        <span>Game List</span>
-        <game-list v-bind:games=games>
+        <div class="mb-10">
+            <h1 class="text-3xl border-black border-b mb-3">Abstract</h1>
+            <p class="text-justify">{{abstract}}</p>
+        </div>
 
-        </game-list>
-
-
+        <div class="mb-10">
+            <h1 class="text-3xl border-black border-b mb-3">Game List</h1>
+            <game-list v-bind:games=games></game-list>
+        </div>
     </div>
 </template>
 
@@ -27,24 +27,19 @@
             return {
                 genreName: "",
                 abstract: "",
+                name: "",
                 games : [],
 
             }
         },
         created() {
             this.genreName = this.$route.params.genreName;
+
             getGenreByName(this.genreName).then(genre => {
                 this.abstract = genre.abstract;
                 this.games = genre.games;
-                /*eslint-disable*/
-                console.log(genre);
+                this.name = genre.name;
             })
-
-
         },
     }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-100 bg-purple-500 flex items-center justify-between border-b-4 border-black">
+    <div class="w-100 bg-purple-500 mb-10 flex items-center justify-between border-b-4 border-black">
         <router-link to="/" class="p-4 rounded-lg text-xl w-1/6 text-black">Accueil</router-link>
         <div class="text-4xl font-bold text-black">{{title}}</div>
         <div class="w-1/6"></div>
