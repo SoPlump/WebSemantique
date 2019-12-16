2e65d9622f66f8a2e675f89993f4a3c830396943
mathieu <matmont98@gmail.com>
2019-12-16 16:14:36 +0100

    tweaked graphics of genre page

--- src/components/GenreView.vue
+++ src/components/GenreView.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="flex flex-col">
         <div class="mb-10">
-            <h1 class="text-4xl p-4 rounded-lg border-black border mb-3">{{name}}</h1>
+            <h1 class="text-4xl p-4 rounded-lg border-purple-700 border-2 mb-3">{{name}}</h1>
         </div>
 
         <div class="mb-10">
-            <h1 class="text-3xl border-black border-b mb-3">Abstract</h1>
+            <h1 class="text-3xl border-purple-700 border-b-2 mb-3">Abstract</h1>
             <p class="text-justify">{{abstract}}</p>
         </div>
 
         <div class="mb-10">
-            <h1 class="text-3xl border-black border-b mb-3">Game List</h1>
+            <h1 class="text-3xl border-purple-700 border-b-2 mb-3">Game List</h1>
             <game-list v-bind:games=games></game-list>
         </div>
     </div>
