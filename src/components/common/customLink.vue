5e85fe2e9decf26898d22f0f28843f7866d4e824
bvachez <brice.vachez@insa-lyon.fr>
2019-12-17 22:03:52 +0100

    Changed <a>s to <router-link>s

--- src/components/common/customLink.vue
+++ src/components/common/customLink.vue
@@ -1,15 +1,18 @@
 <template>
-    <a :href = uri class="align-middle text hover:shadow-xl p-2 rounded-lg hover:bg-purple-200 leading-loose">
+    <router-link :to=uri class="align-middle text hover:shadow-xl p-2 rounded-lg hover:bg-purple-200 leading-loose">
         {{name}} <font-awesome-icon :icon="['fas', 'external-link-alt']" class="text-purple-400 text-xs"></font-awesome-icon>
-    </a>
+    </router-link>
 </template>
 
 <script>
+
     export default {
         name: "customLink",
         props: {
             uri: String,
             name: String,
+        },
+        methods: {
         }
     }
 </script>
